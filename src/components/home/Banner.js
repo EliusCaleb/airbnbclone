20579228d5cca1488753191fd4f20eda1a7c5832
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-05 13:27:14 +0300

    added navigation to the app

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,9 +1,11 @@
 import React,{useState} from 'react'
 import './Banner.css'
 import { Button } from '@material-ui/core'
-import  Search from './Search'
+import  Search from './Search';
+import { useNavigate} from "react-router-dom"
 
 function Banner() {
+    const navigate = useNavigate()
     const [ showSearch,setShowSearch] = useState(false)
   return (
     <div className='banner'>
@@ -20,7 +22,7 @@ function Banner() {
                 <h5>
                     Plan a different kind of getaway to uncover the hidden gems near you.
                 </h5>
-            <Button variant='outlined'>Explore Nearby</Button>
+            <Button variant='outlined' onClick={ ()=> { navigate("/search")}}>Explore Nearby</Button>
        </div>
 
     </div>
--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -5,9 +5,11 @@ import "react-date-range/dist/theme/default.css"
 import { DateRangePicker } from 'react-date-range';
 import { Button } from '@material-ui/core';
 import PeopleIcon from "@material-ui/icons/People"
+import { useNavigate } from 'react-router-dom';
 
 
 function Search() {
+  const navigate = useNavigate()
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date ());
 
@@ -30,7 +32,7 @@ function Search() {
 
        <input  min={0} defaultValue={2} type='number
        '/>
-       <Button>Search AirBnB</Button>
+       <Button onClick={ ()=> navigate("/search")}>Search AirBnB</Button>
     </div>
   )
 }
